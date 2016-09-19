dfa330091f0f6acc11f7bac088980cd8fb82dd45
VitaTaf <BryanTan0204@Gmail.com>
2016-09-18 21:09:32 -0700

    Remove telephony feature
    Change-Id: I01c9bf1febb164b248432adcf288497bbf914282

--- full_ghost.mk
+++ full_ghost.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from ghost device
 $(call inherit-product, device/motorola/ghost/device.mk)
